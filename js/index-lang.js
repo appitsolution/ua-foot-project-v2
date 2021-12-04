507dff9d18f998828e730dfb88e13a39232eccaf
Ruslan <ruslanpetrov163@gmail.com>
2021-12-04 20:59:43 +0200

    correct lang

--- js/index-lang.js
+++ js/index-lang.js
@@ -1,17 +1,25 @@
-const selectLang = document.querySelector(".header-lang-select");
+const selectLang = document.querySelector(".lang-select-button");
 const langList = document.querySelector(".lang-select");
 const langClick = (evn) => {
-  evn.preventDefault();
-  switch (evn.srcElement.textContent) {
-    case "ua":
-      selectLang.textContent = "ua";
-      break;
-    case "ru":
-      selectLang.textContent = "ru";
-      break;
-    case "en":
-      selectLang.textContent = "en";
-      break;
+  if (evn.target.nodeName !== "P") {
+    return
   }
+  const list = [...langList.children];
+
+  list.forEach(num => {
+    num.classList.toggle("close-lang")
+  });
 }
+const selectlan = (evn) => {
+  if (evn.target.nodeName !== "P") {
+    return
+  }
+  if (evn.target.outerText === selectLang.innerText) {
+    return
+  }
+  const name = selectLang.children[0].textContent;
+  selectLang.children[0].textContent = evn.target.outerText;
+  evn.target.textContent = name;
+}
+langList.addEventListener("click", selectlan)
 langList.addEventListener("click", langClick);
